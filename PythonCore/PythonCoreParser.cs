d5964fedde53140c97778175c0989e125b09daf9
Richard Magnor Stenbro <stenbror@hotmail.com>
2024-07-03 21:15:13 +0200

    First tree simple literals implemented.

--- PythonCore/PythonCoreParser.cs
+++ PythonCore/PythonCoreParser.cs
@@ -4,6 +4,12 @@ public sealed class PythonCoreParser
 
     private Symbol _symbol = new Symbol(0, 0);
 
+    public Tuple<uint, uint> CurrentSymbolPosition { get; private set; } = new(0, 0); 
+
+
+
+    private void Advance() { }
+
 
     // Grammar rule: Atom //////////////////////////////////////////////////////////////////////////////////////////////
     public ExpressionNode ParseAtom() => 
@@ -18,17 +24,26 @@ public sealed class PythonCoreParser
 
     private NoneLiteralNode ParseAtomNone()
     {
-        return new NoneLiteralNode(0, 0, _symbol);
+        var pos = CurrentSymbolPosition;
+        var res = new NoneLiteralNode(pos.Item1, pos.Item2, _symbol);
+        Advance();
+        return res;
     }
 
     private FalseLiteralNode ParseAtomFalse()
     {
-        return new FalseLiteralNode(0, 0, _symbol);
+        var pos = CurrentSymbolPosition;
+        var res = new FalseLiteralNode(pos.Item1, pos.Item2, _symbol);
+        Advance();
+        return res;
     }
 
     private TrueLiteralNode ParseAtomTrue()
     {
-        return new TrueLiteralNode(0, 0, _symbol);
+        var pos = CurrentSymbolPosition;
+        var res = new TrueLiteralNode(pos.Item1, pos.Item2, _symbol);
+        Advance();
+        return res;
     }
 
 }
