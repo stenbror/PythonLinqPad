11c20548f403e5a66c99580eb03087e00df0d37c
Richard Magnor Stenbro <stenbror@hotmail.com>
2024-07-03 19:12:58 +0200

    Started on first rule. Atom

--- PythonCore/PythonCoreParser.cs
+++ PythonCore/PythonCoreParser.cs
@@ -1,6 +1,34 @@
 ﻿namespace PythonCore;
-
-public class PythonCoreParser
+public sealed class PythonCoreParser
 {
+
+    private Symbol _symbol = new Symbol(0, 0);
+
+
+    // Grammar rule: Atom //////////////////////////////////////////////////////////////////////////////////////////////
+    public ExpressionNode ParseAtom() => 
+    
+        _symbol switch
+        {
+            PyNone => ParseAtomNone(),
+            PyFalse => ParseAtomFalse(),
+            PyTrue => ParseAtomTrue(),
+            _ => throw new NotImplementedException()
+        };
+
+    private NoneLiteralNode ParseAtomNone()
+    {
+        return new NoneLiteralNode(0, 0, _symbol);
+    }
+
+    private FalseLiteralNode ParseAtomFalse()
+    {
+        return new FalseLiteralNode(0, 0, _symbol);
+    }
+
+    private TrueLiteralNode ParseAtomTrue()
+    {
+        return new TrueLiteralNode(0, 0, _symbol);
+    }
 
 }
--- PythonCore/Symbols.cs
+++ PythonCore/Symbols.cs
@@ -1,3 +1,12 @@
 namespace PythonCore;
 
 public record Symbol(uint StartPos, uint EndPos);
+
+public sealed record PyFalse(uint StartPos, uint EndPos) : Symbol(StartPos, EndPos);
+public sealed record PyNone(uint StartPos, uint EndPos) : Symbol(StartPos, EndPos);
+public sealed record PyTrue(uint StartPos, uint EndPos) : Symbol(StartPos, EndPos);
+public sealed record PyAnd(uint StartPos, uint EndPos) : Symbol(StartPos, EndPos);
+public sealed record PyAs(uint StartPos, uint EndPos) : Symbol(StartPos, EndPos);
+public sealed record PyAssert(uint StartPos, uint EndPos) : Symbol(StartPos, EndPos);
+public sealed record PyAsync(uint StartPos, uint EndPos) : Symbol(StartPos, EndPos);
+public sealed record PyAwait(uint StartPos, uint EndPos) : Symbol(StartPos, EndPos);
