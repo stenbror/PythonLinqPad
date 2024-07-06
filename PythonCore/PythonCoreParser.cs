7bf00396b457d32b628015eae574682305ed3c52
Richard Magnor Stenbro <stenbror@hotmail.com>
2024-07-06 19:17:38 +0200

    Started on atom rule.

--- PythonCore/PythonCoreParser.cs
+++ PythonCore/PythonCoreParser.cs
@@ -19,6 +19,13 @@ public sealed class PythonCoreParser
             PyNone => ParseAtomNone(),
             PyFalse => ParseAtomFalse(),
             PyTrue => ParseAtomTrue(),
+            PyElipsis => ParseAtomElipsis(),
+            PyName => ParseAtomName(),
+            PyNumber => ParseAtomNumber(),
+            PyString => ParseAtomString(),
+            PyLeftParen => ParseAtomTuple(),
+            PyLeftBracket => ParseAtomList(),
+            PyLeftCurly => ParseAtomSetOrDictionary(),
             _ => throw new NotImplementedException()
         };
 
@@ -42,6 +49,62 @@ public sealed class PythonCoreParser
     {
         var pos = CurrentSymbolPosition;
         var res = new TrueLiteralNode(pos.Item1, pos.Item2, _symbol);
+        Advance();
+        return res;
+    }
+
+    private ElipsisLiteralNode ParseAtomElipsis()
+    {
+        var pos = CurrentSymbolPosition;
+        var res = new ElipsisLiteralNode(pos.Item1, pos.Item2, _symbol);
+        Advance();
+        return res;
+    }
+
+    private ElipsisLiteralNode ParseAtomName()
+    {
+        var pos = CurrentSymbolPosition;
+        var res = new ElipsisLiteralNode(pos.Item1, pos.Item2, _symbol);
+        Advance();
+        return res;
+    }
+
+    private ElipsisLiteralNode ParseAtomNumber()
+    {
+        var pos = CurrentSymbolPosition;
+        var res = new ElipsisLiteralNode(pos.Item1, pos.Item2, _symbol);
+        Advance();
+        return res;
+    }
+
+    private ElipsisLiteralNode ParseAtomString()
+    {
+        var pos = CurrentSymbolPosition;
+        var res = new ElipsisLiteralNode(pos.Item1, pos.Item2, _symbol);
+        Advance();
+        return res;
+    }
+
+    private ElipsisLiteralNode ParseAtomTuple()
+    {
+        var pos = CurrentSymbolPosition;
+        var res = new ElipsisLiteralNode(pos.Item1, pos.Item2, _symbol);
+        Advance();
+        return res;
+    }
+
+    private ElipsisLiteralNode ParseAtomList()
+    {
+        var pos = CurrentSymbolPosition;
+        var res = new ElipsisLiteralNode(pos.Item1, pos.Item2, _symbol);
+        Advance();
+        return res;
+    }
+
+    private ElipsisLiteralNode ParseAtomSetOrDictionary()
+    {
+        var pos = CurrentSymbolPosition;
+        var res = new ElipsisLiteralNode(pos.Item1, pos.Item2, _symbol);
         Advance();
         return res;
     }
