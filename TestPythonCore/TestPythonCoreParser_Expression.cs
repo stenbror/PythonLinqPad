186ec84a000619d5cd6e6873d9243e5c5b66a2c3
Richard Magnor Stenbro <stenbror@hotmail.com>
2024-07-08 02:03:10 +0200

    More Atom rule UnitTests passed.

--- TestPythonCore/TestPythonCoreParser_Expression.cs
+++ TestPythonCore/TestPythonCoreParser_Expression.cs
@@ -15,5 +15,58 @@ public class TestPythonCoreParserExpression
         Assert.IsType<ElipsisLiteralNode>(res);
         Assert.Equal(0, res.StartPos);
         Assert.Equal(3, res.EndPos);
+        Assert.IsType<PyElipsis>((res as ElipsisLiteralNode)?.Element);
+    }
+
+    [Fact]
+    public void TestExpressionRuleAtomFalse()
+    {
+        var parser = new PythonCoreParser("False ");
+        parser.Advance();
+        var res = parser.ParseAtom();
+
+        Assert.IsType<FalseLiteralNode>(res);
+        Assert.Equal(0, res.StartPos);
+        Assert.Equal(6, res.EndPos);
+        Assert.IsType<PyFalse>((res as FalseLiteralNode)?.Element);
+    }
+
+    [Fact]
+    public void TestExpressionRuleAtomNone()
+    {
+        var parser = new PythonCoreParser("None ");
+        parser.Advance();
+        var res = parser.ParseAtom();
+
+        Assert.IsType<NoneLiteralNode>(res);
+        Assert.Equal(0, res.StartPos);
+        Assert.Equal(5, res.EndPos);
+        Assert.IsType<PyNone>((res as NoneLiteralNode)?.Element);
+    }
+
+    [Fact]
+    public void TestExpressionRuleAtomTrue()
+    {
+        var parser = new PythonCoreParser("True ");
+        parser.Advance();
+        var res = parser.ParseAtom();
+
+        Assert.IsType<TrueLiteralNode>(res);
+        Assert.Equal(0, res.StartPos);
+        Assert.Equal(5, res.EndPos);
+        Assert.IsType<PyTrue>((res as TrueLiteralNode)?.Element);
+    }
+
+    [Fact]
+    public void TestExpressionRuleAtomName()
+    {
+        var parser = new PythonCoreParser("__init__ ");
+        parser.Advance();
+        var res = parser.ParseAtom();
+
+        Assert.IsType<NameLiteralNode>(res);
+        Assert.Equal(0, res.StartPos);
+        Assert.Equal(9, res.EndPos);
+        Assert.IsType<PyName>((res as NameLiteralNode)?.Element);
     }
 }
