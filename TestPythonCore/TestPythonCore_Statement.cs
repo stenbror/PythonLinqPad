a66155ed95ae49d4a6c4d1a9ee2420cce0d9c021
Richard Magnor Stenbro <stenbror@hotmail.com>
2024-07-18 12:13:50 +0200

    Passed unittest for try/except statement.

--- TestPythonCore/TestPythonCore_Statement.cs
+++ TestPythonCore/TestPythonCore_Statement.cs
@@ -974,5 +974,33 @@ namespace TestPythonCore
 
             Assert.Equivalent(required, res, strict: true);
         }
+
+        [Fact]
+        public void TestStatementTryExceptStatement()
+        {
+            var parser = new PythonCoreParser("try: pass\r\nexcept: pass\r\n\r\n");
+            parser.Advance();
+            var res = parser.ParseStmts();
+
+            var required = new TryExceptFinallyStatementBlockNode(0, 25,
+                new PyTry(0, 3, []),
+                new PyColon(3, 4, []),
+                new SimpleStmtsNode(5, 11, [
+                    new PassStmtNode(5, 9, new PyPass(5, 9, [new WhiteSpaceTrivia(4, 5)]))
+                ], [], new PyNewline(9, 11, '\r', '\n', [])),
+                [
+                    new DefaultExceptStatementNode(11, 25,
+                        new PyExcept(11, 17, []),
+                        new PyColon(17, 18, []),
+                        new SimpleStmtsNode(19, 25, [
+                            new PassStmtNode(19, 23, new PyPass(19, 23, [new WhiteSpaceTrivia(18, 19)]))
+                        ], [], new PyNewline(23, 25, '\r', '\n', [])))
+                ],
+                null,
+                null
+            );
+
+            Assert.Equivalent(required, res, strict: true);
+        }
     }
 }
